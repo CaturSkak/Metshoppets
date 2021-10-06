d5cf1b0dc0d2793775f94020aa930ef7b96ab0c8
Ahmad Catur Yulianto <66737336+CaturSkak@users.noreply.github.com>
2021-10-06 10:24:19 +0700

    Update header.php
    update menu

--- amber/header.php
+++ amber/header.php
@@ -56,11 +56,11 @@
                 <div class="row align-items-center">
 
                     <!-- Header Top Message Start -->
-                    <center>
+                     <center>
                     <div class="col-12 col-lg-6">
                         <div class="header-top-msg-wrapper">
                             
-                            <p class="header-top-message">Welcome To MetShopPets</p>
+                            <p class="header-top-message">Selamat Datang di MetShopPets</p>
                             
                         </div>
                     </center>
@@ -88,15 +88,17 @@
                         <!-- Header Logo End -->
 
                         <!-- Header Menu Start -->
-                        <div class="col-lg-6 d-none d-lg-block">
+                       <div class="col-lg-6 d-none d-lg-block">
                             <div class="main-menu">
                                 <ul>
                                     <li class="has-children">
-                                        <a href="index.php">Home</a>
-                                        
+                                        <a href="#">Beranda</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="index.php">Beranda</a></li>
+                                        </ul>
                                     </li>
                                     <li class="has-children position-relative">
-                                        <a href="#">Shop</a>
+                                        <a href="#">Belanja</a>
                                         <ul class="mega-menu">
                                             <li class="mega-menu-col">
                                                 <h4 class="mega-menu-title">Katagori</h4>
@@ -109,12 +111,12 @@
                                                 <li class="mega-menu-col">
                                                 <h4 class="mega-menu-title">Lainya</h4>
                                                 <ul class="mb-n2">
-                                                    <li><a href="my-account.php">My Account</a></li>
-                                                    <li><a href="login.php">Login | Register</a></li>
-                                                    <li><a href="wishlist.php">Wishlist</a></li>
-                                                    <li><a href="cart.php">Cart</a></li>
+                                                    <li><a href="my-account.php">Akun saya</a></li>
+                                                    <li><a href="login.php">Masuk | Daftar</a></li>
+                                                    <li><a href="wishlist.php">Daftar Keinginan</a></li>
+                                                    <li><a href="cart.php">Keranjang</a></li>
                                                     <li><a href="checkout.php">Checkout</a></li>
-                                                    <li><a href="compare.php">Compare</a></li>
+                                                    <li><a href="compare.php">Perbanding</a></li>
                                                 </ul>
                                             </li>
                                             <li class="mega-menu-col">
@@ -127,11 +129,11 @@
                                         </ul>
                                     </li>
                                     <li class="has-children">
-                                        <a href="#">Pages</a>
+                                        <a href="#">Halaman</a>
                                         <ul class="sub-menu">
                                             <li><a href="faq.php">FAQ</a></li>
-                                            <li><a href="login.php">Login</a></li>
-                                            <li><a href="register.php">Register</a></li>
+                                            <li><a href="login.php">Masuk</a></li>
+                                            <li><a href="register.php">Daftar</a></li>
                                         </ul>
                                     </li>
                                     <li class="has-children">
@@ -143,151 +145,9 @@
                                             <li><a href="blog-details.php">Burung</a></li>
                                         </ul>
                                     </li>
-                                    <li><a href="about.php">About</a></li>
-                                    <li><a href="contact.php">Contact</a></li>
+                                    <li><a href="about.php">Tentang</a></li>
+                                    <li><a href="contact.php">Kontak</a></li>
                                 </ul>
                             </div>
                         </div>
                         <!-- Header Menu End -->
-
-                        <!-- Header Action Start -->
-                        <div class="col-lg-3 col-md-8 col-6">
-                            <div class="header-actions">
-
-                                <!-- Header Action Search Button Start -->
-                                <div class="header-action-btn header-action-btn-search d-none d-md-flex">
-                                    <div class="action-execute">
-                                        <a class="action-search-open" href="javascript:void(0)"><i class="icon-magnifier icons"></i></a>
-                                        <a class="action-search-close" href="javascript:void(0)"><i class="ti-close"></i></a>
-                                    </div>
-                                    <!-- Search Form and Button Start -->
-                                    <form class="header-search-form" action="#">
-                                        <input type="text" class="header-search-input" placeholder="Search Our Store">
-                                        <button class="header-search-button"><i class="icon-magnifier icons"></i></button>
-                                    </form>
-                                    <!-- Search Form and Button End -->
-
-                                </div>
-                                <!-- Header Action Search Button End -->
-
-                                <!-- Header My Account Button Start -->
-                                <a href="my-account.php" class="header-action-btn header-action-btn-wishlist">
-                                    <i class="icon-user icons"></i>
-                                </a>
-                                <!-- Header My Account Button End -->
-
-                                <!-- Header Action Button Start -->
-                                <div class="header-action-btn header-action-btn-cart d-none d-sm-flex">
-                                    <a class="cart-visible" href="javascript:void(0)">
-                                        <i class="icon-handbag icons"></i>
-                                        <span class="header-action-num">3</span>
-                                    </a>
-
-                                    <!-- Header Cart Content Start -->
-                                    <div class="header-cart-content">
-
-                                        <!-- Cart Procut Wrapper Start  -->
-                                        <div class="cart-product-wrapper">
-
-                                            <!-- Cart Product/Price Start -->
-                                            <div class="cart-product-inner mb-4 pb-4 border-bottom">
-
-                                                <!-- Single Cart Product Start -->
-                                                <div class="single-cart-product">
-                                                    <div class="cart-product-thumb">
-                                                        <a href="single-product.php"><img src="assets/images/header/header-cart/1.png" alt="Cart Product"></a>
-                                                    </div>
-                                                    <div class="cart-product-content">
-                                                        <h3 class="title"><a href="single-product.php">Basic Dog Trainning</a></h3>
-                                                        <div class="product-quty-price">
-                                                            <span class="cart-quantity">Qty: <strong class="text-primary"> 1 </strong></span>
-                                                            <span class="price">
-															<span class="new">$70.00</span>
-                                                            </span>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <!-- Single Cart Product End -->
-
-                                                <!-- Product Remove Start -->
-                                                <div class="cart-product-remove">
-                                                    <a href="#/"><i class="icon-close"></i></a>
-                                                </div>
-                                                <!-- Product Remove End -->
-
-                                            </div>
-                                            <!-- Cart Product/Price End -->
-
-                                            <!-- Cart Product/Price Start -->
-                                            <div class="cart-product-inner mb-4 pb-4 border-bottom">
-
-                                                <!-- Single Cart Product Start -->
-                                                <div class="single-cart-product">
-                                                    <div class="cart-product-thumb">
-                                                        <a href="single-product.php"><img src="assets/images/header/header-cart/2.png" alt="Cart Product"></a>
-                                                    </div>
-                                                    <div class="cart-product-content">
-                                                        <h3 class="title"><a href="single-product.php">Wait, You Need This</a></h3>
-                                                        <div class="product-quty-price">
-                                                            <span class="cart-quantity">Qty: <strong class="text-primary"> 1 </strong></span>
-                                                            <span class="price">
-															<span class="new">$80.00</span>
-                                                            </span>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <!-- Single Cart Product End -->
-
-                                                <!-- Product Remove Start -->
-                                                <div class="cart-product-remove">
-                                                    <a href="#/"><i class="icon-close"></i></a>
-                                                </div>
-                                                <!-- Product Remove End -->
-
-                                            </div>
-                                            <!-- Cart Product/Price End -->
-
-                                        </div>
-                                        <!-- Cart Procut Wrapper -->
-
-                                        <!-- Cart Product Total Start -->
-                                        <div class="cart-product-total mb-4 pb-4 border-bottom">
-                                            <span class="value">Total</span>
-                                            <span class="price">200$</span>
-                                        </div>
-                                        <!-- Cart Product Total End -->
-
-                                        <!-- Cart Product Button Start -->
-                                        <div class="cart-product-btn mt-4">
-                                            <a href="cart.php" class="btn btn-outline-light btn-hover-primary w-100">View cart</a>
-                                            <a href="checkout.php" class="btn btn-outline-light btn-hover-primary w-100 mt-4">Checkout</a>
-                                        </div>
-                                        <!-- Cart Product Button End -->
-
-                                    </div>
-                                    <!-- Header Cart Content End -->
-
-                                </div>
-                                <div class="header-action-btn header-action-btn-cart d-flex d-sm-none">
-                                    <a href="cart.php">
-                                        <i class="icon-handbag icons"></i>
-                                        <span class="header-action-num">3</span>
-                                    </a>
-                                </div>
-                                <!-- Header Action Button End -->
-
-                                <!-- Mobile Menu Hambarger Action Button Start -->
-                                <a href="javascript:void(0)" class="header-action-btn header-action-btn-menu d-lg-none d-md-flex">
-                                    <i class="icon-menu"></i>
-                                </a>
-                                <!-- Mobile Menu Hambarger Action Button End -->
-
-                            </div>
-                        </div>
-                        <!-- Header Action End -->
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Header Bottom End -->
