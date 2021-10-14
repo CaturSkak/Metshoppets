8c222f152006446a16eacb20844622f762020c9a
FarhanJulystio <80513156+FarhanJulystio@users.noreply.github.com>
2021-10-14 14:05:27 +0700

    new2

---
+++ amber/chat.php
@@ -0,0 +1,193 @@
+<link rel="stylesheet" href="assets/css/chat.css">
+<div id="container">
+  <aside>
+    <header>
+      <input type="text" placeholder="Cari">
+    </header>
+    <ul>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_01.jpg" alt="">
+        <div>
+          <h2>Doni</h2>
+          <h3>
+            <span class="status green"></span>
+            online
+          </h3>
+        </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_02.jpg" alt="">
+        <div>
+          <h2>Joni</h2>
+          <h3>
+            <span class="status green"></span>
+            online
+          </h3>
+        </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_03.jpg" alt="">
+        <div>
+          <h2>Alex</h2>
+          <h3>
+            <span class="status orange"></span>
+            offline
+          </h3>
+        </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_04.jpg" alt="">
+        <div>
+          <h2>Siva</h2>
+          <h3>
+            <span class="status green"></span>
+            online
+          </h3>
+        </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_05.jpg" alt="">
+        <div>
+          <h2>Serly</h2>
+          <h3>
+            <span class="status orange"></span>
+            offline
+          </h3>
+        </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_06.jpg" alt="">
+        <div>
+          <h2>Vita</h2>
+          <h3>
+            <span class="status green"></span>
+            online
+          </h3>
+        </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_07.jpg" alt="">
+        <div>
+          <h2>Deni</h2>
+          <h3>
+            <span class="status green"></span>
+            online
+          </h3>
+        </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_08.jpg" alt="">
+        <div>
+          <h2>Norita</h2>
+          <h3>
+            <span class="status green"></span>
+            online
+          </h3>
+        </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_09.jpg" alt="">
+        <div>
+          <h2>Dita</h2>
+          <h3>
+            <span class="status green"></span>
+            online
+          </h3>
+        </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_10.jpg" alt="">
+        <div>
+          <h2>Citra</h2>
+          <h3>
+            <span class="status orange"></span>
+            offline
+          </h3>
+        </div>
+      </li>
+    </ul>
+  </aside>
+  <main>
+    <header>
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_01.jpg" alt="">
+      <div>
+        <h2>Doni</h2>
+        <h3>2 Pesan</h3>
+      </div>
+    </header>
+    <ul id="chat">
+      <li class="you">
+        <div class="entete">
+          <span class="status green"></span>
+          <h2>Doni</h2>
+          <h3>10:20, Hari Ini</h3>
+        </div>
+        <div class="triangle"></div>
+        <div class="message">
+          Kamu Lagi Sibuk Tidak Hari ini.
+        </div>
+      </li>
+      <li class="me">
+        <div class="entete">
+          <h3>10:12, Hari Ini</h3>
+          <h2>Anda</h2>
+          <span class="status blue"></span>
+        </div>
+        <div class="triangle"></div>
+        <div class="message">
+          Tidak Sih, Ada Apa Emangnya ?
+        </div>
+      </li>
+      <li class="you">
+        <div class="entete">
+          <span class="status green"></span>
+          <h2>Doni</h2>
+          <h3>10:15, Hari Ini</h3>
+        </div>
+        <div class="triangle"></div>
+        <div class="message">
+          Ngopi Yuks !
+        </div>
+      </li>
+      <li class="me">
+        <div class="entete">
+          <span class="status blue"></span>
+          <h3>10:15, Hari Ini</h3>
+          <h2>Anda</h2>
+        </div>
+        <div class="triangle"></div>
+        <div class="message">
+          Ok, Siap. Tunggu Bentar Saya Siap Siap Dulu !
+        </div>
+      </li>
+      <li class="you">
+        <div class="entete">
+          <h2>Doni</h2>
+          <h3>10:17, Hari Ini</h3>
+          <span class="status green"></span>
+        </div>
+        <div class="triangle"></div>
+        <div class="message">
+          Ok, Saya Tunggu Di Cafe Yang biasa aja ya.
+        </div>
+      </li>
+      <li class="me">
+        <div class="entete">
+          <h3>10:18, Hari Ini</h3>
+          <h2>Anda</h2>
+          <span class="status blue"></span>
+        </div>
+        <div class="triangle"></div>
+        <div class="message">
+          OK, Siap.
+        </div>
+      </li>
+    </ul>
+    <footer>
+      <textarea placeholder="Kirim Pesan"></textarea>
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_picture.png" alt="">
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_file.png" alt="">
+      <a href="#">Kirim</a>
+    </footer>
+  </main>
+</div>
