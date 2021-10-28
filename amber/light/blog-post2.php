6caf26878b18e89d4f1cd35aa0a177bc00b0bfbe
FarhanJulystio <80513156+FarhanJulystio@users.noreply.github.com>
2021-10-28 14:34:11 +0700

    new2

--- amber/light/blog-post2.php
+++ amber/light/blog-post2.php
@@ -13,10 +13,10 @@
 <!-- VENDOR CSS -->
 <link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="../assets/vendor/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="../assets/vendor/summernote/dist/summernote.css"/>
 
 <!-- MAIN CSS -->
 <link rel="stylesheet" href="assets/css/main.css">
-<link rel="stylesheet" href="assets/css/blog.css">
 <link rel="stylesheet" href="assets/css/color_skins.css">
 </head>
 <body class="theme-orange">
@@ -419,120 +419,85 @@
         <div class="block-header">
             <div class="row clearfix">
                 <div class="col-md-6 col-sm-12">
-                    <h2>Artikel</h2>
+                    <h2>Buat Artikel Baru</h2>
                 </div>            
                 <div class="col-md-6 col-sm-12 text-right">
                     <ul class="breadcrumb">
                         <li class="breadcrumb-item"><a href="index.php"><i class="icon-home"></i></a></li>
-                        <li class="breadcrumb-item active">Artikel</li>
+                        <li class="breadcrumb-item active"><a href="blog-list.php">Artikel</li></a>
                     </ul>
-                    <a href="blog-post.php" class="btn btn-sm btn-primary" title="">Buat Baru</a>
                 </div>
             </div>
         </div>
         <div class="container-fluid">
+           
             <div class="row clearfix">
-                <div class="col-lg-20 col-15">
-                    <div class="row row-cols-lg-2 row-cols-sm-2 row-cols-5 mb-n5">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="body">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Cara Menjaga Kesehatan Hewan Peliharaan" />
+                            </div>
 
-                    <div class="col mb-8">
-                    <div class="single-blog-wrapper">
-                        <div class="blog-thumb thumb-effect">
-                            <img class="img-fluid" src="../assets/images/121.jpg" alt="img">
+                            <div class="form-group">
+                                <select class="form-control show-tick">
+                                    <option>Hewan Peliharaan</option>
+                                    <option>Hewan Peliharaan</option>
+                                    <option>Burung</option>
+                                    <option>Ikan</option>
+                                    <option>Kesehatan Hewan</option>
+                                    <option>Vaksinasi Hewan</option>
+                                </select>
+                            </div>
+                            <div class="summernote">
+                                <img class="img-fluid" src="../assets/images/121.jpg" alt="img">
+                                <h3 class="m-b-0">Halo dengan Admin,</h3>
+                                <h4 class="m-t-0">Wabah COVID-19 tak hanya menginfeksi manusia, tetapi juga hewan-hewan. Beberapa penemuan kasus hewan positif covid ditemukan di dunia. Di antaranya harimau malaya di Kebun Binatang Bronx, New York, Amerika Serikat, seekor anjing di Hong Kong, dan dua kucing di New York. Kasus ini terjadi akibat penularan dari manusia.</h4>
+                                <p></p>
+                            </div>
+                            <button type="button" class="btn btn-block btn-primary m-t-20">Edit</button>
                         </div>
-
-                        <div class="blog-content">
-                            <div class="content">
-                                <h4 class="title">Cara Menjaga Kesehatan Hewan Peliharaan</h4>
-                                <p class="date">
-                                    <small>15, Juli, 2021</small>
-                                </p>
-                                <p class="text">Wabah COVID-19 tak hanya menginfeksi manusia, tetapi  juga hewan-hewan. Beberapa penemuan kasus hewan positif covid ditemukan di dunia, Di antaranya...</p>
-                                <a class="btn btn-primary" href="blog-post2.php">Edit</a>
-                                <a class="btn btn-primary" href="">Hapus</a>
-                                <a class="btn btn-primary" href="">Tidak Ditampilkan</a>
-                                <i class="btn fa fa-eye" aria-hidden="true" href=""></i>
-                        </div>                    
                     </div>
-                </div>
+                </div>            
             </div>
-                <div class="col mb-8 col-17 col-md-6">
-                    <div class="single-blog-wrapper">
-                        <img class="img-fluid" src="../assets/images/111.jpg" alt="img">
-                        <div class="blog-content">                    
-                            <div class="content">
-                                <h4 class="title">Tips Pemberian Pakan Ikan Hias</h4>
-                                <p class="date">
-                                    <small>07, September, 2013</small>
-                                </p>
-                                <p class="text">Pemakaian pakan buatan untuk ikan hias mesti dikerjakan dengan benar serta hati-hati agar pertumbuhannya bisa berjalan maksimal. Karena diinginkan tak lagi...</p>
-                                <a class="btn btn-primary" href="blog-post2.php">Edit</a>
-                                <a class="btn btn-primary" href="">Hapus</a>
-                                <a class="btn btn-primary" href="javascript:void(0)">Perlihatkan</a>
-                                <a class="btn btn-primary" href="">Tidak Ditampilkan</a>
-                                <i class="btn fa fa-eye" aria-hidden="true" href=""></i>
-                            </div>
-                        </div>                        
-                    </div>
-                </div>
 
-                <div class="col mb-8">
-                    <div class="single-blog-wrapper">
-                        <img class="img-fluid" src="../assets/images/131.jpg" alt="img">
-                        <div class="body">                    
-                            <div class="content">
-                                <h4 class="title">15 Cara Merawat Burung</h4>
-                                <p class="date">
-                                    <small>17, Februari, 2018</small>
-                                </p>
-                                <p class="text">Memiliki burung yang rajin berkicau tentunya sangat diinginkan setiap pemilik burung. Sebenarnya ada 3 faktor yang bisa meningkatkan burung rajin berkicau yakni karakter atau trah, pola perawatan dan lingkungan dan juga pakan serta minuman...</p>
-                                <a class="btn btn-primary" href="blog-post2.php">Edit</a>
-                                <a class="btn btn-primary" href="">Hapus</a>
-                                <a class="btn btn-primary" href="javascript:void(0)">Perlihatkan</a>
-                                <i class="btn fa fa-eye-slash" href=""></i>
-                            </div>
-                        </div>                        
-                    </div>
-                </div>
-                    
-                <div class="col mb-8">
-                    <div class="single-blog-wrapper">
-                        <img class="img-fluid" src="../assets/images/212.jpg" alt="img">
-                        <div class="body">                    
-                            <div class="content">
-                                <h4 class="title">Mengenal Vaksin Penting untuk Hewan Peliharaan</h4>
-                                <p class="date">
-                                    <small>08, Februari, 2021</small>
-                                </p>
-                                <p class="text">Sebagai pemilik kucing, fenomena kucing mengalami kejang pasti mengerikan. Kamu harus mengenali apa saja penyebab kucing kejang agar bisa mengatasinya dengan mudah. Walaupun begitu, tidak selalu kejang yang terjadi pada..</p>
-                                <a class="btn btn-primary" href="blog-post2.php">Edit</a>
-                                <a class="btn btn-primary" href="">Hapus</a>
-                                <a class="btn btn-primary" href="javascript:void(0)">Perlihatkan</a>
-                                <a class="btn btn-primary" href="">Tidak Ditampilkan</a>
-                                <i class="btn fa fa-eye" aria-hidden="true" href=""></i>
-                            </div>
-                        </div>                        
-                    </div>
-                </div>
-                                 
-                    <ul class="pagination pagination-primary">
-                        <li class="page-item"><a class="page-link" href="javascript:void(0);">Previous</a></li>
-                        <li class="page-item active"><a class="page-link" href="javascript:void(0);">1</a></li>
-                        <li class="page-item"><a class="page-link" href="javascript:void(0);">2</a></li>
-                        <li class="page-item"><a class="page-link" href="javascript:void(0);">3</a></li>
-                        <li class="page-item"><a class="page-link" href="javascript:void(0);">Next</a></li>
-                    </ul>               
-                </div>
         </div>
     </div>
-    </div>
-
+    
 </div>
 
 <!-- Javascript -->
 <script src="assets/bundles/libscripts.bundle.js"></script>    
 <script src="assets/bundles/vendorscripts.bundle.js"></script>
 
+
 <script src="assets/bundles/mainscripts.bundle.js"></script>
+<script src="../assets/vendor/summernote/dist/summernote.js"></script>
+
+<script>
+    jQuery(document).ready(function() {
+
+        $('.summernote').summernote({
+            height: 350, // set editor height
+            minHeight: null, // set minimum height of editor
+            maxHeight: null, // set maximum height of editor
+            focus: false, // set focus to editable area after initializing summernote
+            popover: { image: [], link: [], air: [] }
+        });
+
+        $('.inline-editor').summernote({
+            airMode: true
+        });
+
+    });
+
+    window.edit = function() {
+            $(".click2edit").summernote()
+        },
+        
+    window.save = function() {
+        $(".click2edit").summernote('destroy');
+    }
+</script>
 </body>
 </html>
